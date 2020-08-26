ef32abf19b9cb27907fdb28db378fff10b1c9852
Trio <triosaputra308@gmail.com>
2020-08-26 19:59:45 +0700

    new updated

---
+++ database/migrations/2020_08_26_122658_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->String("fullName");
+            $table->String("email");
+            $table->String("phoneNumber");
+            $table->String("password");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
