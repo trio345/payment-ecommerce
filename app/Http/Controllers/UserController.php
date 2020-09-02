d6e798490e4d7a2f903853c49d3322ae3ac3c96b
Trio <triosaputra308@gmail.com>
2020-09-02 15:24:55 +0700

    new updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
 
                 
         } else {
-            return response($content = ["status" => false, "messages" => "failed login wrong email or password", "data" => []], 322);
+            return response($content = ["status" => false, "messages" => "failed login wrong email or password"], 304);
         }
     }
 }
